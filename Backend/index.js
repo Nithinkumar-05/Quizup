a90f0fbe2a6ad996d8a413dba287462cba1bca78
NagaKarthikeyan <nagakarthikeyan18@gmail.com>
2024-10-17 22:47:26 +0530

    minor changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,32 @@
 const express = require("express");
 const connection = require("./connection/database");
-const port = 1000 || process.env.PORT;
+const port = process.env.PORT || 1000;
 const app = express();
 const cors = require("cors");
 const loginRoute = require("./routes/login.route");
 const quizRoute = require("./routes/quiz.route");
 const resultRoute = require("./routes/result.route");
-// const quizRoute = require("./routes/quiz.route");
 // const userRoute = require("./routes/user.route");
 // const questionRoute = require("./routes/question.route");
 // const resultRoute = require("./routes/result.route");
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(cors());
 
+// Middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://quized.vercel.app", // Only allow requests from your frontend
+  })
+);
+
+// Routes
 app.use("/quiz", loginRoute);
-app.use("/host",quizRoute);
-app.use("/contest",resultRoute);
+app.use("/host", quizRoute);
+app.use("/contest", resultRoute);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
